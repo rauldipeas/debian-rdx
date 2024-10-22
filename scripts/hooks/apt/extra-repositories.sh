cedc39b4c2ec357db826aa3fb122b45a95bb0b2f
Raul Dipeas <rauldipeas@disroot.org>
2024-10-22 12:11:57 -0300

    ^extra-repositories

--- scripts/hooks/apt/extra-repositories.sh
+++ scripts/hooks/apt/extra-repositories.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 # Google Chrome
-wget -qO- https://dl.google.com/linux/linux_signing_key.pub|sudo gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg>/dev/null
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main'|sudo tee /etc/apt/sources.list.d/google-chrome.list>/dev/null
-# Mozilla Firefox
-wget -qO- https://packages.mozilla.org/apt/repo-signing-key.gpg|sudo gpg --dearmor -o /usr/share/keyrings/packages.mozilla.org.gpg>/dev/null
-echo 'deb [signed-by=/usr/share/keyrings/packages.mozilla.org.gpg] https://packages.mozilla.org/apt mozilla main'|sudo tee /etc/apt/sources.list.d/mozilla.list>/dev/null
+wget -qO- https://dl.google.com/linux/linux_signing_key.pub|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/google-chrome.gpg>/dev/null
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main'|sudo tee /etc/apt/sources.list.d/google-chrome.list>/dev/null
 # KXStudio
 wget -cq --show-progress http://ppa.launchpad.net/kxstudio-debian/kxstudio/ubuntu/pool/main/k/kxstudio-repos/"$(wget -qO- http://ppa.launchpad.net/kxstudio-debian/kxstudio/ubuntu/pool/main/k/kxstudio-repos/|grep all.deb|tail -n1|cut -d '"' -f8)"
 sudo apt install ./kxstudio-repos*.deb
 rm ./kxstudio-repos*.deb
+# MEGA
+wget -qO- https://mega.nz/keys/MEGA_signing.key|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mega.gpg>/dev/null
+echo 'deb https://mega.nz/linux/repo/Debian_12/ ./'|sudo tee /etc/apt/sources.list.d/mega.list>/dev/null
+# Mozilla Firefox
+wget -qO- https://packages.mozilla.org/apt/repo-signing-key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mozilla.gpg>/dev/null
+echo 'deb https://packages.mozilla.org/apt mozilla main'|sudo tee /etc/apt/sources.list.d/mozilla.list>/dev/null
 # Raul Dipeas
 bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
